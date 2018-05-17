2663e36aeb67eb81c5bfe3733fb9fc5a92e89644
xttjsn <xttjsn@gmail.com>
2018-05-17 17:37:37 -0400

    Change compilation target to shared library

--- Makefile
+++ Makefile
@@ -1,10 +1,36 @@
-all: gc lang
+BUILD_DIR      := build
+GC_LIB         := $(BUILD_DIR)/lib/libgash_gc.a
+LANG_LIB       := $(BUILD_DIR)/lib/libgash_lang.a
+MIRACL_LIB     := $(BUILD_DIR)/lib/libmiracl.a
+GASH_SLIB      := $(BUILD_DIR)/lib/libgash.so
+HEAD           := $(parser_HEAD) $(lexer_HEAD) $(wildcard *.hh)
+SRC            := $(parser_CSRC) $(lexer_CSRC) $(filter-out main.cc, $(wildcard *.cc))
+OBJS           := $(addsuffix .o,$(basename $(SRC)))
+INSTALL_PREFIX := /usr/local
 
-gc:
+CXX            := g++
+CXXFLAGS       := -std=c++11 -pthread -Wno-ignored-attributes -g -fPIC
+
+all: gc lang $(GASH_SLIB) test
+
+gc $(GC_LIB) $(MIRACL_LIB):
 	@ cd gc && make
 
-lang:
+lang $(LANG_LIB):
 	@ cd lang && make
+
+$(GASH_SLIB): $(GC_LIB) $(LANG_LIB) $(MIRACL_LIB)
+	@ echo "    Building libgash.so"
+	@ echo "$(CXX) $^ -shared -o $@"
+	@ $(CXX) -o $@ -shared -Wl,--whole-archive $^ -Wl,--no-whole-archive
+
+test:
+	@ echo "    Building tests"
+	@ cd test && make
+
+install: $(GASH_SLIB)
+	@ echo "cp $(GASH_SLIB) $(INSTALL_PREFIX)/lib"
+	@ cp $(GASH_SLIB) $(INSTALL_PREFIX)/lib
 
 cscope:
 	@ find . -name "*.[ch]" > cscope.files
@@ -20,4 +46,4 @@ clean:
 	@ cd lang && $(MAKE) clean
 	@ cd gc && $(MAKE) clean
 
-.PHONY: all gc lang clean cscope clang-format
+.PHONY: all gc lang test clean cscope clang-format
--- gc/build_miracl.sh
+++ gc/build_miracl.sh
@@ -10,64 +10,64 @@ cd ${OTExtEncryptoPath}/miracl_lib ;
 rm *.exe
 rm miracl.a
 cp mirdef.h64 mirdef.h
-gcc -c -m64 -O2 mrcore.c
-gcc -c -m64 -O2 mrarth0.c
-gcc -c -m64 -O2 mrarth1.c
-gcc -c -m64 -O2 mrarth2.c
-gcc -c -m64 -O2 mralloc.c
-gcc -c -m64 -O2 mrsmall.c
-gcc -c -m64 -O2 mrio1.c
-gcc -c -m64 -O2 mrio2.c
-gcc -c -m64 -O2 mrgcd.c
-gcc -c -m64 -O2 mrjack.c
-gcc -c -m64 -O2 mrxgcd.c
-gcc -c -m64 -O2 mrarth3.c
-gcc -c -m64 -O2 mrbits.c
-gcc -c -m64 -O2 mrrand.c
-gcc -c -m64 -O2 mrprime.c
-gcc -c -m64 -O2 mrcrt.c
-gcc -c -m64 -O2 mrscrt.c
-gcc -c -m64 -O2 mrmonty.c
-gcc -c -m64 -O2 mrpower.c
-gcc -c -m64 -O2 mrsroot.c
-gcc -c -m64 -O2 mrcurve.c
-gcc -c -m64 -O2 mrfast.c
-gcc -c -m64 -O2 mrshs.c
-gcc -c -m64 -O2 mrshs256.c
-gcc -c -m64 -O2 mrshs512.c
-gcc -c -m64 -O2 mrsha3.c
-gcc -c -m64 -O2 mrfpe.c
-gcc -c -m64 -O2 mraes.c
-gcc -c -m64 -O2 mrgcm.c
-gcc -c -m64 -O2 mrlucas.c
-gcc -c -m64 -O2 mrzzn2.c
-gcc -c -m64 -O2 mrzzn2b.c
-gcc -c -m64 -O2 mrzzn3.c
-gcc -c -m64 -O2 mrzzn4.c
-gcc -c -m64 -O2 mrecn2.c
-gcc -c -m64 -O2 mrstrong.c
-gcc -c -m64 -O2 mrbrick.c
-gcc -c -m64 -O2 mrebrick.c
-gcc -c -m64 -O2 mrec2m.c
-gcc -c -m64 -O2 mrgf2m.c
-gcc -c -m64 -O2 mrflash.c
-gcc -c -m64 -O2 mrfrnd.c
-gcc -c -m64 -O2 mrdouble.c
-gcc -c -m64 -O2 mrround.c
-gcc -c -m64 -O2 mrbuild.c
-gcc -c -m64 -O2 mrflsh1.c
-gcc -c -m64 -O2 mrpi.c
-gcc -c -m64 -O2 mrflsh2.c
-gcc -c -m64 -O2 mrflsh3.c
-gcc -c -m64 -O2 mrflsh4.c
+gcc -fPIC -c -m64 -O2 mrcore.c
+gcc -fPIC -c -m64 -O2 mrarth0.c
+gcc -fPIC -c -m64 -O2 mrarth1.c
+gcc -fPIC -c -m64 -O2 mrarth2.c
+gcc -fPIC -c -m64 -O2 mralloc.c
+gcc -fPIC -c -m64 -O2 mrsmall.c
+gcc -fPIC -c -m64 -O2 mrio1.c
+gcc -fPIC -c -m64 -O2 mrio2.c
+gcc -fPIC -c -m64 -O2 mrgcd.c
+gcc -fPIC -c -m64 -O2 mrjack.c
+gcc -fPIC -c -m64 -O2 mrxgcd.c
+gcc -fPIC -c -m64 -O2 mrarth3.c
+gcc -fPIC -c -m64 -O2 mrbits.c
+gcc -fPIC -c -m64 -O2 mrrand.c
+gcc -fPIC -c -m64 -O2 mrprime.c
+gcc -fPIC -c -m64 -O2 mrcrt.c
+gcc -fPIC -c -m64 -O2 mrscrt.c
+gcc -fPIC -c -m64 -O2 mrmonty.c
+gcc -fPIC -c -m64 -O2 mrpower.c
+gcc -fPIC -c -m64 -O2 mrsroot.c
+gcc -fPIC -c -m64 -O2 mrcurve.c
+gcc -fPIC -c -m64 -O2 mrfast.c
+gcc -fPIC -c -m64 -O2 mrshs.c
+gcc -fPIC -c -m64 -O2 mrshs256.c
+gcc -fPIC -c -m64 -O2 mrshs512.c
+gcc -fPIC -c -m64 -O2 mrsha3.c
+gcc -fPIC -c -m64 -O2 mrfpe.c
+gcc -fPIC -c -m64 -O2 mraes.c
+gcc -fPIC -c -m64 -O2 mrgcm.c
+gcc -fPIC -c -m64 -O2 mrlucas.c
+gcc -fPIC -c -m64 -O2 mrzzn2.c
+gcc -fPIC -c -m64 -O2 mrzzn2b.c
+gcc -fPIC -c -m64 -O2 mrzzn3.c
+gcc -fPIC -c -m64 -O2 mrzzn4.c
+gcc -fPIC -c -m64 -O2 mrecn2.c
+gcc -fPIC -c -m64 -O2 mrstrong.c
+gcc -fPIC -c -m64 -O2 mrbrick.c
+gcc -fPIC -c -m64 -O2 mrebrick.c
+gcc -fPIC -c -m64 -O2 mrec2m.c
+gcc -fPIC -c -m64 -O2 mrgf2m.c
+gcc -fPIC -c -m64 -O2 mrflash.c
+gcc -fPIC -c -m64 -O2 mrfrnd.c
+gcc -fPIC -c -m64 -O2 mrdouble.c
+gcc -fPIC -c -m64 -O2 mrround.c
+gcc -fPIC -c -m64 -O2 mrbuild.c
+gcc -fPIC -c -m64 -O2 mrflsh1.c
+gcc -fPIC -c -m64 -O2 mrpi.c
+gcc -fPIC -c -m64 -O2 mrflsh2.c
+gcc -fPIC -c -m64 -O2 mrflsh3.c
+gcc -fPIC -c -m64 -O2 mrflsh4.c
 cp mrmuldv.g64 mrmuldv.c
-gcc -c -m64 -O2 mrmuldv.c
+gcc -fPIC -c -m64 -O2 mrmuldv.c
 ####
-g++ -c -m64 -O2 big.cpp
-g++ -c -m64 -O2 zzn.cpp
-g++ -c -m64 -O2 ecn.cpp
-g++ -c -m64 -O2 ec2.cpp
-g++ -c -m64 -O2 crt.cpp
+g++ -fPIC -c -m64 -O2 big.cpp
+g++ -fPIC -c -m64 -O2 zzn.cpp
+g++ -fPIC -c -m64 -O2 ecn.cpp
+g++ -fPIC -c -m64 -O2 ec2.cpp
+g++ -fPIC -c -m64 -O2 crt.cpp
 ####
 ar rc miracl.a mrcore.o mrarth0.o mrarth1.o mrarth2.o mralloc.o mrsmall.o mrzzn2.o mrzzn3.o
 ar r miracl.a mrio1.o mrio2.o mrjack.o mrgcd.o mrxgcd.o mrarth3.o mrbits.o mrecn2.o mrzzn4.o
@@ -80,23 +80,23 @@ ar r miracl.a mrbrick.o mrebrick.o mrec2m.o mrgf2m.o mrmuldv.o mrshs512.o mrsha3
 ar r miracl.a big.o zzn.o ecn.o ec2.o crt.o
 ####
 rm mr*.o
-gcc -m64 -O2 bmark.c miracl.a -o bmark
-gcc -m64 -O2 fact.c miracl.a -o fact
+gcc -fPIC -m64 -O2 bmark.c miracl.a -o bmark
+gcc -fPIC -m64 -O2 fact.c miracl.a -o fact
 # g++ -c -m64 -O2 big.cpp
 # g++ -c -m64 -O2 zzn.cpp
 # g++ -c -m64 -O2 ecn.cpp
 # g++ -c -m64 -O2 ec2.cpp
 # g++ -c -m64 -O2 crt.cpp
-g++ -m64 -O2 mersenne.cpp big.o miracl.a -o mersenne
-g++ -m64 -O2 brent.cpp big.o zzn.o miracl.a -o brent
-g++ -c -m64 -O2 flash.cpp
-g++ -m64 -O2 sample.cpp flash.o miracl.a -o sample
-g++ -m64 -O2 ecsgen.cpp ecn.o big.o miracl.a -o ecsgen
-g++ -m64 -O2 ecsign.cpp ecn.o big.o miracl.a -o ecsign
-g++ -m64 -O2 ecsver.cpp ecn.o big.o miracl.a -o ecsver
-g++ -m64 -O2 pk-demo.cpp ecn.o big.o miracl.a -o pk-demo
-g++ -c -m64 -O2 polymod.cpp
-g++ -c -m64 -O2 poly.cpp
-g++ -m64 -O2 schoof.cpp polymod.o poly.o ecn.o crt.o zzn.o big.o miracl.a -o schoof
+g++ -fPIC -m64 -O2 mersenne.cpp big.o miracl.a -o mersenne
+g++ -fPIC -m64 -O2 brent.cpp big.o zzn.o miracl.a -o brent
+g++ -fPIC -c -m64 -O2 flash.cpp
+g++ -fPIC -m64 -O2 sample.cpp flash.o miracl.a -o sample
+g++ -fPIC -m64 -O2 ecsgen.cpp ecn.o big.o miracl.a -o ecsgen
+g++ -fPIC -m64 -O2 ecsign.cpp ecn.o big.o miracl.a -o ecsign
+g++ -fPIC -m64 -O2 ecsver.cpp ecn.o big.o miracl.a -o ecsver
+g++ -fPIC -m64 -O2 pk-demo.cpp ecn.o big.o miracl.a -o pk-demo
+g++ -fPIC -c -m64 -O2 polymod.cpp
+g++ -fPIC -c -m64 -O2 poly.cpp
+g++ -fPIC -m64 -O2 schoof.cpp polymod.o poly.o ecn.o crt.o zzn.o big.o miracl.a -o schoof
 
 # find . -type f -not -name '*.a' -not -name '*.h' -not -name '*.o' -not -name '.git*' | xargs rm
