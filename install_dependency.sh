ecd4e8f91972c54697bb83f2be1c2826e6260daa
xtt <xtt@localhost.localdomain>
2018-05-29 11:46:33 -0400

    Add dependency installation script for Fedora

---
+++ install_dependency.sh
@@ -0,0 +1,1 @@
+sudo dnf install -y boost.x86_64 boost-devel.x86_64 bison.x86_64 bison-devel.x86_64 flex.x86_64 flex-devel.x86_64 openssl.x86_64 openssl-libs.x86_64 openssl-devel.x86_64 boost.x86_64 boost-devel.x86_64 gtest.x86_64 gtest-devel.x86_64 gmp.x86_64 gmp-devel.x86_64 gcc-c++.x86_64
