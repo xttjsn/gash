d250a11cfa9b038f845ab223271e8244718a9762
Jason <xttjsn@gmail.com>
2018-06-17 15:44:27 -0400

    Initial Commit

---
+++ gash/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  gash
+//
+//  Created by Xiaoting Tang on 6/17/18.
+//  Copyright © 2018 Xiaoting Tang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ gash/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  gash
+//
+//  Created by Xiaoting Tang on 6/17/18.
+//  Copyright © 2018 Xiaoting Tang. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
